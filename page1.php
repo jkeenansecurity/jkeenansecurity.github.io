93098d85c160bb4afbbd117a4b02fab7d8249295
Jared <jared@jkeenansecurity.com>
2015-10-25 22:49:16 -0400

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,78 @@
+<!DOCTYPE HTML>
+<html lang='en-US'>
+<head>
+    <title>JKeenanSecurity</title>
+    <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+    <link href="main.css" rel="stylesheet" />
+    <meta charset="utf-8" />
+    <meta name="description" content="JKeenan Security Service">
+    </head>
+<body>
+    <header>
+    <div id='logodiv'>
+    <a id ='logo' href='index.html'><img src='logo.png' alt='logo' width='50%'height='200'></a>
+        </div>
+    <nav>
+        <ul>
+            <li><a class = 'navbutton' href='index.html'>Home</a></li>
+            <li><a id='home'class = 'navbutton' href='page1.html'>Contact Us</a></li>
+        </ul>
+    </nav>
+    </header>
+    <aside id='sidebar'>
+        <section>
+        <h2>Contact Us</h2>
+            <p>Give us a call today and speak with one of our professional security consulatants
+            who will find out what you're looking for and devise a plan to match.
+            </p>
+        
+        
+        </section>
+        <br />
+    <section>
+        <h2>Payment Options Include</h2>
+        <ul>
+        <li class = 'card'><img src='mastercard.png' alt='Mastercard'width='90'height='50'</li>
+        <li class = 'card'><img src='visa.jpg'width='90' height = '50' alt='Visa'</li>
+        <li class = 'card'><img src='amex.jpg'width='90' height = '50' alt='American Express'</li>
+        <li class = 'card'><img src='discover.gif'width='90'height='50' alt='Discover'</li>
+        <li class = 'card' id='debit'><img src='debit.jpg' width='98'height='50'alt='Debit'</li>
+        </ul>
+        </section>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <hr />
+        
+        <section>
+        <h2>Call us today at: (800)705-8894</h2>
+        
+        </section>
+    </aside>
+    <section class = 'maincontent'>
+        <header>
+        <img src ='nyc2.png' alt='New York City'class='sectionheader'height='600'width='100%'/>
+        </header>
+    <article>
+    <iframe src='Contact/contactform.php' frameborder='0'
+width='100%' height='600' allowtransparency='true'></iframe>
+
+    </article>
+    </section>
+    <br />
+    
+    
+
+    
+    
+    
+    
+    </body>
+
+
+
+
+</html>
